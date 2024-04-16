37e2031e39fad4b306ee7b6f6db195163f98ae61
Cissoko420 <59943539+Cissoko420@users.noreply.github.com>
2024-04-16 13:35:48 +0100

    Uploaded closeAccounts.sh
    Close Token Accounts for ORE

---
+++ closeAccounts.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nr_wallets=60
+
+clear
+
+for((i=4;i<=nr_wallets;i++)); do
+	balance=$(ore --keypair ~/.config/solana/ids/id$i.json balance | grep -oP '^\d+(.\d)?(?= ORE)')
+	if [ ! -z "$balance" ] && [ "$(echo "balance == 0" | bc)" -eq 1 ]; then
+		echo "ID$i balance: $balance ORE - Close account"
+		solana config set --keypair /root/.config/solana/ids/id$i.json
+		spl-token close --fee-payer /root/.config/solana/ids/id$i.json oreoN2tQbHXVaZsr3pf66A48miqcBXCDJozganhEJgz --owner /root/.config/solana/id$i.json
+		echo ""
+		sleep 2
+	else
+		echo "ID$i balance: $balance ORE - Unable to close account"
+		echo ""
+	fi
+done
